a47ba542b7bf6affff2f135e547137d16eb804ac
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-05-30 18:48:29 +0200

    Initial commit

---
+++ src/main/java/com/example/medontes5/Medontes5Application.java
@@ -0,0 +1,13 @@
+package com.example.medontes5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medontes5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Medontes5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medontes5/Medontes5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medontes5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medontes5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
