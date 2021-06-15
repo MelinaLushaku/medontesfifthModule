fcd6aa765c553fd7158c37f7a29130ea49998135
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-06-16 00:54:25 +0200

    ApiGateway

--- src/main/java/com/example/medontes5/Medontes5Application.java
+++ src/main/java/com/example/medontes5/Medontes5Application.java
@@ -2,8 +2,11 @@ package com.example.medontes5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Medontes5Application {
 
     public static void main(String[] args) {
