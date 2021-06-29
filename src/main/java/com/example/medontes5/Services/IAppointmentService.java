350fa0a34562092662cc38f9421ec270b9f412a3
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-06-30 00:54:09 +0200

    DiagnosisFunctions

--- src/main/java/com/example/medontes5/Services/IAppointmentService.java
+++ src/main/java/com/example/medontes5/Services/IAppointmentService.java
@@ -10,19 +10,36 @@ import java.util.List;
 public interface IAppointmentService {
 
     void addNewFreeAppointments(Appointment a);
+
     DoctorEntity getDoctorByPrNumber(int nrPersonal);
+
     PatientEntity getPatientByPrNumber(int nrPersonal);
-    void editAppointment(int doc , Date data , PatientEntity p, float time);
+
+    void editAppointment(int doc, Date data, PatientEntity p, float time);
+
     List<Appointment> getAppByDoc(int doc);
+
     List<Appointment> getAppByPat(int pat);
-    void cancelAppointment(int doc , Date data, float time);
-    void deleteAppointment(int doc , Date date, float time , int pat );
+
+    void cancelAppointment(int doc, Date data, float time);
+
+    void deleteAppointment(int doc, Date date, float time, int pat);
+
     List<Appointment> todayAppDoc(int doc);
+
     List<Appointment> todayAppPat(int pat);
-    List<Appointment> byTime(int idD , float time);
-    List<Appointment> listaTP(int doc , Date date ,float time, int pat);
-    List<Appointment>listTM(int doc , Date data , float time);
+
+    List<Appointment> byTime(int idD, float time);
+
+    List<Appointment> listaTP(int doc, Date date, float time, int pat);
+
+    List<Appointment> listTM(int doc, Date data, float time);
+
     int getAllApp();
+
     int nextDays();
 
+
 }
+
+
